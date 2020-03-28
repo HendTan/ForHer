0abb9fcb13da298dd5ac77fcad838cad81a08e9e
Hend <xinzhao0318@gmail.com>
2020-03-28 16:01:04 +0800

    added login function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import ImageCar from './component/Carousel'
-import StrContent from './component/Words'
+import Login from './Pages/LoginPg'
 import './App.css';
 
 function App() {
   return (
     <div className="App" style={{backgroundColor:'black'}}>
-      <ImageCar />
-      <StrContent />
+      <Login />
     </div>
   );
 }
---
+++ src/Pages/LoginPg.js
@@ -0,0 +1,65 @@
+import React, { useState,useRef } from 'react'
+
+import ImageCar from '../component/Carousel'
+import StrContent from '../component/Words'
+
+const Login = () => {
+
+    const username = useRef('')
+    const password = useRef('')
+    const [loggedIn,setLoggedIn] = useState(false)
+    const [disabled,setEnabled] = useState(false)
+    const handleOnChange = () => {
+        if(username.current.value.length < 5 || password.current.value.length < 5){
+            setEnabled(false)
+        }else{
+            setEnabled(true)
+        }
+    }
+    const subBtn = (e) => {
+        e.preventDefault()
+        if(username.current.value === 'zixin0427' || password.current.value === 'zixin0427'){
+            setLoggedIn(true)
+        }else{
+            username.current.value = ''
+            password.current.value = ''
+            setLoggedIn(false)
+            alert('Something wrong there. Please type the username and password again')
+        }
+        
+    }
+    return (
+        <div>
+            {loggedIn? 
+                <div>
+                    <ImageCar />
+                    <StrContent />
+                </div>:
+                <div>
+                    <form >
+                        <input type='text' ref={username} onChange={handleOnChange} style={{
+                            borderRadius: '10px',
+                            position: 'relative',
+                            top: '100px',
+                            left: '0px'
+                        }}/>
+                        <input type='password'  ref={password} onChange={handleOnChange} style={{
+                            borderRadius: '10px',
+                            position: 'relative',
+                            top: '150px',
+                            left: '-180px'
+                        }}/>
+                        <button type='submit' onClick={subBtn} disabled={!disabled} style={{
+                            borderRadius: '10px',
+                            position: 'relative',
+                            top: '200px',
+                            left: '-300px'
+                        }}>Login</button> 
+                    </form>
+                </div>
+            }
+        </div>
+    )
+}
+
+export default Login
